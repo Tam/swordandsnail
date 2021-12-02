3f1437d7429404855fe829696ace549a97bca7af
Tam <hi@tam.sx>
2021-12-02 01:24:10 +0000

    Add post login redirect

--- web/pages/_middleware.js
+++ web/pages/_middleware.js
@@ -21,13 +21,15 @@ const AGNOSTIC_URLS = [
 export async function middleware (req) {
 	const ssrid = req.cookies['snail.ssrid'];
 
-	const isProtectedUrl = PUBLIC_URLS.indexOf(req.url) === -1 && AGNOSTIC_URLS.indexOf(req.url) === -1
-		, isPublicUrl = PUBLIC_URLS.indexOf(req.url) !== -1;
+	const isProtectedUrl = PUBLIC_URLS.indexOf(req.url) === -1 && AGNOSTIC_URLS.indexOf(req.url) === -1;
 
 	// If we don't have an SSRID
 	if (!ssrid) {
-		if (isProtectedUrl)
-			return NextResponse.redirect(LOGIN_URL);
+		if (isProtectedUrl) {
+			const res = NextResponse.redirect(LOGIN_URL);
+			res.cookie('snail.post_login', req.url, { secure: true, httpOnly: true });
+			return res;
+		}
 
 		return NextResponse.next();
 	}
@@ -46,12 +48,23 @@ export async function middleware (req) {
 	// If verification failed
 	if (!resp?.data?.verifySsrid) {
 		let res;
-		if (isProtectedUrl) res = NextResponse.redirect(LOGIN_URL)
-		else if (isPublicUrl) res = NextResponse.redirect(POST_LOGIN_URL)
+		if (isProtectedUrl) {
+			res = NextResponse.redirect(LOGIN_URL);
+			res.cookie('snail.post_login', req.url, { secure: true, httpOnly: true });
+		}
 		else res = NextResponse.next();
 
 		// Clear the SSRID cookie
 		res.cookie('snail.ssrid', '', { maxAge: 0, secure: true, httpOnly: true });
+
+		return res;
+	}
+
+	const postLoginRedirect = req.cookies['snail.post_login'];
+
+	if (postLoginRedirect) {
+		const res = NextResponse.redirect(postLoginRedirect);
+		res.cookie('snail.post_login', '', { maxAge: 0, secure: true, httpOnly: true });
 
 		return res;
 	}
