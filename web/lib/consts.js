0fda9308d7e3e3c3dbfab3a605dbd20a594a5919
Tam <hi@tam.sx>
2021-12-02 00:24:00 +0000

    Revert URL / trace middleware errors

--- web/lib/consts.js
+++ web/lib/consts.js
@@ -1,3 +1,1 @@
-export const URI = process.env.NODE_ENV === 'development'
-	? 'https://dev.api.swordandsnail.com/graphql'
-	: 'https://api.swordandsnail.com/graphql';
+export const URI = process.env.NEXT_PUBLIC_API ?? 'https://dev.api.swordandsnail.com/graphql';
--- web/pages/_middleware.js
+++ web/pages/_middleware.js
@@ -19,42 +19,46 @@ const AGNOSTIC_URLS = [
 ];
 
 export async function middleware (req) {
-	const ssrid = req.cookies['snail.ssrid'];
+	try {
+		const ssrid = req.cookies['snail.ssrid'];
 
-	const isProtectedUrl = UNAUTHED_URLS.indexOf(req.url) === -1 && AGNOSTIC_URLS.indexOf(req.url) === -1
-		, isUnauthedUrl = UNAUTHED_URLS.indexOf(req.url) !== -1;
+		const isProtectedUrl = UNAUTHED_URLS.indexOf(req.url) === -1 && AGNOSTIC_URLS.indexOf(req.url) === -1
+			, isUnauthedUrl = UNAUTHED_URLS.indexOf(req.url) !== -1;
 
-	// If we don't have an SSRID
-	if (!ssrid) {
-		if (isProtectedUrl)
-			return NextResponse.redirect(LOGIN_URL);
+		// If we don't have an SSRID
+		if (!ssrid) {
+			if (isProtectedUrl)
+				return NextResponse.redirect(LOGIN_URL);
+
+			return NextResponse.next();
+		}
+
+		// Verify the SSRID
+		const resp = await fetch(URI, {
+			method: 'POST',
+			body: JSON.stringify({
+				operationName: 'VerifySSRID',
+				query: 'query VerifySSRID ($id: String!) { verifySsrid(ssrid: $id) }',
+				variables: { id: ssrid },
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		}).then(r => r.json());
+
+		// If verification failed
+		if (!resp?.data?.verifySsrid) {
+			let res;
+			if (isProtectedUrl) res = NextResponse.redirect(LOGIN_URL)
+			else if (isUnauthedUrl) res = NextResponse.redirect(POST_LOGIN_URL)
+			else res = NextResponse.next();
+
+			// Clear the SSRID cookie
+			res.cookie('snail.ssrid', '', { maxAge: 0, secure: true, httpOnly: true });
+
+			return res;
+		}
 
 		return NextResponse.next();
+	} catch (e) {
+		console.trace(e);
 	}
-
-	// Verify the SSRID
-	const resp = await fetch(URI, {
-		method: 'POST',
-		body: JSON.stringify({
-			operationName: 'VerifySSRID',
-			query: 'query VerifySSRID ($id: String!) { verifySsrid(ssrid: $id) }',
-			variables: { id: ssrid },
-		}),
-		headers: { 'Content-Type': 'application/json' },
-	}).then(r => r.json());
-
-	// If verification failed
-	if (!resp?.data?.verifySsrid) {
-		let res;
-		if (isProtectedUrl) res = NextResponse.redirect(LOGIN_URL)
-		else if (isUnauthedUrl) res = NextResponse.redirect(POST_LOGIN_URL)
-		else res = NextResponse.next();
-
-		// Clear the SSRID cookie
-		res.cookie('snail.ssrid', '', { maxAge: 0, secure: true, httpOnly: true });
-
-		return res;
-	}
-
-	return NextResponse.next();
 }
