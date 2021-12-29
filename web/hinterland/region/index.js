01378a6640e33ad3a20876566c771982d8a2f84b
Tam <hi@tam.sx>
2021-12-29 18:19:01 +0000

    Cellular automata

--- web/hinterland/region/index.js
+++ web/hinterland/region/index.js
@@ -1,5 +1,6 @@
 import css from './style.module.scss';
 import { useEffect, useRef } from 'react';
+import randomFromArray from '../../util/random';
 
 export default function Region () {
 	const self = useRef();
@@ -50,65 +51,104 @@ export default function Region () {
 		const player = { char: '@', position: { x: 0, y: 0 } };
 		drawChar(player);
 
-		const map = [];
+		function generateMap () {
+			const map = [];
 
-		for (let y = 0, l = grid.height; y < l; y++) {
-			for (let x = 0, l = grid.width; x < l; x++) {
-				map.push(Math.random() > 0.75 ? {
-					char: '🌳',
-					position: { x, y },
-				} : null);
+			for (let y = 0, l = grid.height; y < l; y++) {
+				for (let x = 0, l = grid.width; x < l; x++) {
+					if (x <= 1 || x >= grid.width - 2 || y <= 1 || y >= grid.height - 2 || Math.random() > 0.4) {
+						map.push({
+							char: randomFromArray(['🌲','🌴','🌳']),
+							position: { x, y },
+						});
+					} else {
+						map.push(null);
+					}
+				}
 			}
+
+			let iterations = 5;
+			while (iterations--) {
+				for (let y = 0, l = grid.height; y < l; y++) {
+					for (let x = 0, l = grid.width; x < l; x++) {
+						if (map[x + grid.width * y] === null) continue;
+
+						let neighbourCount = 0;
+
+						for (let ny = y - 1; ny <= y + 1; ny++) {
+							for (let nx = x - 1; nx <= x + 1; nx++) {
+								// if (nx < 0 || nx >= grid.width || ny < 0 || ny >= grid.height) continue;
+								if (nx === x && ny === y) continue;
+								if (map[nx + grid.width * ny] !== null) neighbourCount++;
+							}
+						}
+
+						if (neighbourCount < 4) map[x + grid.width * y] = null;
+						// else map[x + grid.width * y].char = neighbourCount;
+					}
+				}
+			}
+
+			clearCanvas();
+			map.forEach(drawChar);
 		}
 
-		const onUserInput = e => {
-			let { x, y } = player.position;
+		generateMap();
 
-			switch (e.key) {
-				case 'ArrowUp':
-					y -= 1;
-					break;
-				case 'ArrowDown':
-					y += 1;
-					break;
-				case 'ArrowLeft':
-					x -= 1;
-					break;
-				case 'ArrowRight':
-					x += 1;
-					break;
-			}
-
-			if (x >= grid.width) x = 0;
-			if (x < 0) x = grid.width - 1;
-			if (y >= grid.height) y = 0;
-			if (y < 0) y = grid.height - 1;
-			
-			if (map[x + grid.width * y] === null) {
-				player.position.x = x;
-				player.position.y = y;
-			}
-		};
-
-		document.addEventListener('keydown', onUserInput);
-
-		let looping = true;
-
-		const loop = () => {
-			clearCanvas();
-
-			map.forEach(drawChar);
-			drawChar(player);
-
-			looping && requestAnimationFrame(loop);
-		};
-
-		loop();
+		document.addEventListener('click', generateMap);
 
 		return () => {
-			looping = false;
-			document.removeEventListener('keydown', onUserInput);
+			document.removeEventListener('click', generateMap);
 		};
+
+		// const onUserInput = e => {
+		// 	let { x, y } = player.position;
+		//
+		// 	switch (e.key) {
+		// 		case 'ArrowUp':
+		// 			y -= 1;
+		// 			break;
+		// 		case 'ArrowDown':
+		// 			y += 1;
+		// 			break;
+		// 		case 'ArrowLeft':
+		// 			x -= 1;
+		// 			break;
+		// 		case 'ArrowRight':
+		// 			x += 1;
+		// 			break;
+		// 	}
+		//
+		// 	if (x >= grid.width) x = 0;
+		// 	if (x < 0) x = grid.width - 1;
+		// 	if (y >= grid.height) y = 0;
+		// 	if (y < 0) y = grid.height - 1;
+		//
+		// 	if (map[x + grid.width * y] === null) {
+		// 		player.position.x = x;
+		// 		player.position.y = y;
+		// 	}
+		// };
+		//
+		// document.addEventListener('keydown', onUserInput);
+		//
+		// let looping = true;
+		//
+		// const loop = () => {
+		// 	clearCanvas();
+		//
+		// 	map.forEach(drawChar);
+		// 	drawChar(player);
+		//
+		// 	looping && requestAnimationFrame(loop);
+		// };
+		//
+		// loop();
+		//
+		// return () => {
+		// 	looping = false;
+		// 	document.removeEventListener('keydown', onUserInput);
+		// };
 	}, [self]);
 
 	return (
