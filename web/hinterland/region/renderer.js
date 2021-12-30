1f76d3797a0b2d4ccc8c21c069ac8b03b5ffdf49
Tam <hi@tam.sx>
2021-12-30 01:07:24 +0000

    Add clear cell funcs to renderer

--- web/hinterland/region/renderer.js
+++ web/hinterland/region/renderer.js
@@ -41,23 +41,39 @@ export default class Renderer {
 		this._ctx.textBaseline = "middle";
 	}
 
+	cellToPos = (x, y) => ({
+		x: x * this._cellWidth + this._cellWidth / 2,
+		y: y * this._cellHeight + this._cellHeight / 2,
+	});
+
+	cellToPosCorner = (x, y) => {
+		const pos = this.cellToPos(x, y);
+
+		return {
+			x: pos.x - this._cellWidth / 2,
+			y: pos.y - this._cellHeight / 2,
+		};
+	};
+
 	clearCanvas = () => {
 		this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
 	}
 
 	clearCell = (x, y) => {
-		this._ctx.clearRect(x, y, this._cellWidth, this._cellHeight);
+		const pos = this.cellToPosCorner(x, y);
+		this._ctx.clearRect(pos.x, pos.y, this._cellWidth, this._cellHeight);
 	}
 
 	drawChar = (c) => {
 		if (!c) return;
 		const { char, position } = c;
+		const pos = this.cellToPos(position.x, position.y);
 
 		this._ctx.fillStyle = this._textColour;
 		this._ctx.fillText(
 			char,
-			position.x * this._cellWidth + this._cellWidth / 2,
-			position.y * this._cellHeight + this._cellHeight / 2
+			pos.x,
+			pos.y
 		);
 	}
 
