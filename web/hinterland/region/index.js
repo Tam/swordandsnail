7b653358766766b84ef61399908da76da9e5619a
Tam <hi@tam.sx>
2021-12-30 18:56:31 +0000

    Add camera zoom and pan

--- web/hinterland/region/index.js
+++ web/hinterland/region/index.js
@@ -20,61 +20,34 @@ export default function Region () {
 		if (!renderer) return;
 
 		forest(renderer, GRID_WIDTH, GRID_HEIGHT);
+		renderer.blit();
 
-		// const player = { char: '@', position: { x: 0, y: 0 } };
-		// drawChar(player);
+		const onKeyPress = e => {
+			switch (e.key) {
+				case 'i':
+					renderer.setZoom(zoom => zoom + 0.25);
+					break;
+				case 'k':
+					renderer.setZoom(zoom => zoom - 0.25);
+					break;
+				case 'w': renderer.moveCamera(0, 1); break;
+				case 's': renderer.moveCamera(0, -1); break;
+				case 'a': renderer.moveCamera(1, 0); break;
+				case 'd': renderer.moveCamera(-1, 0); break;
+			}
+		};
 
-		// const onUserInput = e => {
-		// 	let { x, y } = player.position;
-		//
-		// 	switch (e.key) {
-		// 		case 'ArrowUp':
-		// 			y -= 1;
-		// 			break;
-		// 		case 'ArrowDown':
-		// 			y += 1;
-		// 			break;
-		// 		case 'ArrowLeft':
-		// 			x -= 1;
-		// 			break;
-		// 		case 'ArrowRight':
-		// 			x += 1;
-		// 			break;
-		// 	}
-		//
-		// 	if (x >= grid.width) x = 0;
-		// 	if (x < 0) x = grid.width - 1;
-		// 	if (y >= grid.height) y = 0;
-		// 	if (y < 0) y = grid.height - 1;
-		//
-		// 	if (map[x + grid.width * y] === null) {
-		// 		player.position.x = x;
-		// 		player.position.y = y;
-		// 	}
-		// };
-		//
-		// document.addEventListener('keydown', onUserInput);
-		//
-		// let looping = true;
-		//
-		// const loop = () => {
-		// 	clearCanvas();
-		//
-		// 	map.forEach(drawChar);
-		// 	drawChar(player);
-		//
-		// 	looping && requestAnimationFrame(loop);
-		// };
-		//
-		// loop();
-		//
-		// return () => {
-		// 	looping = false;
-		// 	document.removeEventListener('keydown', onUserInput);
-		// };
+		document.addEventListener('keydown', onKeyPress);
+
+		return () => {
+			document.removeEventListener('keydown', onKeyPress);
+		};
 	}, [renderer]);
 
-	const onGenerateClick = () => forest(renderer, GRID_WIDTH, GRID_HEIGHT);
+	const onGenerateClick = () => {
+		forest(renderer, GRID_WIDTH, GRID_HEIGHT);
+		renderer.blit();
+	};
 
 	return (
 		<div className={css.wrap}>
--- web/hinterland/region/renderer.js
+++ web/hinterland/region/renderer.js
@@ -5,10 +5,15 @@ export default class Renderer {
 	_width;
 	_height;
 
+	_zoom = 1;
+	_camera = { x: 0, y: 0 };
+
 	_cellWidth;
 	_cellHeight;
 
 	_textColour;
+
+	_buffer = [];
 
 	constructor (canvas, width = 100, height = 50) {
 		this._canvas = canvas;
@@ -18,74 +23,192 @@ export default class Renderer {
 
 		this._textColour = window.getComputedStyle(document.body).getPropertyValue('color');
 
+		this._calculateSizing();
+	}
+
+	_calculateSizing = () => {
 		const lineHeight = 1.2
 			, pixelRatio = window.devicePixelRatio || 1;
 
-		const calculatedFontSize = window.innerWidth / width;
+		const calculatedFontSize = window.innerWidth / this._width;
 		const cellWidth = calculatedFontSize * pixelRatio
 			, cellHeight = calculatedFontSize * lineHeight * pixelRatio
-			, fontSize = calculatedFontSize * pixelRatio;
+			, fontSize = calculatedFontSize * pixelRatio * this._zoom;
 
-		this._cellWidth = cellWidth;
-		this._cellHeight = cellHeight;
+		this._cellWidth = cellWidth * this._zoom;
+		this._cellHeight = cellHeight * this._zoom;
 
 		this._canvas.style.cssText = `
-			width: ${calculatedFontSize * width}; 
-			height: ${calculatedFontSize * lineHeight * height};
+			width: ${calculatedFontSize * this._width}; 
+			height: ${calculatedFontSize * lineHeight * this._height};
 		`;
-		this._canvas.width = cellWidth * width;
-		this._canvas.height = cellHeight * height;
+		this._canvas.width = cellWidth * this._width;
+		this._canvas.height = cellHeight * this._height;
 
 		this._ctx.font = `normal ${fontSize}px ${window.getComputedStyle(document.body).getPropertyValue('font-family').split(',')[0]}`;
 		this._ctx.textAlign = 'center';
 		this._ctx.textBaseline = 'middle';
 	}
 
-	cellToPos = (x, y) => ({
-		x: x * this._cellWidth,
-		y: y * this._cellHeight,
+	// Viewport
+	// =========================================================================
+
+	setZoom = valueOrCallback => {
+		if (typeof valueOrCallback === 'function')
+			valueOrCallback = valueOrCallback(this._zoom);
+
+		if (valueOrCallback < 1) valueOrCallback = 1;
+		else if (valueOrCallback > 3) valueOrCallback = 3;
+
+		// TODO: Move the camera to keep it centered
+		//  1. get the current center cell
+		//  2. find the cell offset
+		//  3. calculate the new cell offset
+		//  4. set the zoom
+		//  5. set the new offset
+
+		this._zoom = valueOrCallback;
+		this._calculateSizing();
+		this._clampCamera();
+		this.blit();
+	}
+
+	moveCamera = (x = 0, y = 0) => {
+		this._camera.x += x;
+		this._camera.y += y;
+		this._clampCamera();
+		this.blit();
+	}
+
+	_clampCamera = () => {
+		if (this._camera.x > 0) this._camera.x = 0;
+		if (this._camera.y > 0) this._camera.y = 0;
+
+		// TODO: clamp right / bottom
+		// TODO: move around center of screen rather than top left
+	}
+
+	// Rendering
+	// =========================================================================
+
+	clearCanvas = () => {
+		this._buffer = [];
+		this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+	}
+
+	clearCell = (x, y) => {
+		this._buffer.push([
+			'clearCell',
+			{ x, y },
+		]);
+	}
+
+	drawChar = (char, x, y) => {
+		if (!char) return;
+
+		this._buffer.push([
+			'drawChar',
+			{ char, x, y },
+		]);
+	}
+
+	drawDebugLine = (ax, ay, bx, by) => {
+		this._buffer.push([
+			'drawDebugLine',
+			{ ax, ay, bx, by }
+		]);
+	}
+
+	blit = () => {
+		requestAnimationFrame(this._blit.bind(this));
+	}
+
+	_blit () {
+		this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+
+		this._buffer.forEach(([key, args]) => {
+			switch (key) {
+				case 'clearCell': {
+					const pos = this._cellToPos(args.x, args.y);
+
+					if (this._cullCellByPos(pos))
+						return;
+
+					this._ctx.clearRect(
+						pos.x,
+						pos.y,
+						this._cellWidth,
+						this._cellHeight
+					);
+					break;
+				}
+				case 'drawChar': {
+					const pos = this._cellCentreToPos(args.x, args.y);
+
+					if (this._cullCellByPos(pos))
+						return;
+
+					this._ctx.fillStyle = this._textColour;
+					this._ctx.fillText(
+						args.char,
+						pos.x,
+						pos.y
+					);
+					break;
+				}
+				case 'drawDebugLine': {
+					const { ax, ay, bx, by } = args;
+
+					const a = this._cellCentreToPos(ax, ay)
+						, b = this._cellCentreToPos(bx, by);
+
+					if (
+						a.x && b.x >= this._canvas.width
+						|| a.y && b.y >= this._canvas.height
+						|| a.x && b.x <= 0
+						|| a.y && b.y <= 0
+					) return;
+
+					this._ctx.beginPath();
+					this._ctx.moveTo(a.x, a.y);
+					this._ctx.lineTo(b.x, b.y);
+					this._ctx.lineWidth = this._cellWidth / 5;
+					this._ctx.strokeStyle = 'magenta';
+					this._ctx.stroke();
+					break;
+				}
+			}
+		});
+	}
+
+	// Helpers
+	// =========================================================================
+
+	// Helpers: Render
+	// -------------------------------------------------------------------------
+
+	_cullCellByPos = pos => (
+		pos.x >= this._canvas.width
+		|| pos.y >= this._canvas.height
+		|| pos.x + this._cellWidth <= 0
+		|| pos.y + this._cellHeight <= 0
+	)
+
+	// Helpers: Position
+	// -------------------------------------------------------------------------
+
+	_cellToPos = (x, y) => ({
+		x: x * this._cellWidth + this._camera.x * this._cellWidth,
+		y: y * this._cellHeight + this._camera.y * this._cellHeight,
 	});
 
-	cellCentreToPos = (x, y) => {
-		const pos = this.cellToPos(x, y);
+	_cellCentreToPos = (x, y) => {
+		const pos = this._cellToPos(x, y);
 
 		return {
 			x: pos.x + this._cellWidth / 2,
 			y: pos.y + this._cellHeight / 2,
 		};
 	};
-
-	clearCanvas = () => {
-		this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
-	}
-
-	clearCell = (x, y) => {
-		const pos = this.cellToPos(x, y);
-		this._ctx.clearRect(pos.x, pos.y, this._cellWidth, this._cellHeight);
-	}
-
-	drawChar = (char, x, y) => {
-		if (!char) return;
-		const pos = this.cellCentreToPos(x, y);
-
-		this._ctx.fillStyle = this._textColour;
-		this._ctx.fillText(
-			char,
-			pos.x,
-			pos.y
-		);
-	}
-
-	drawDebugLine = (ax, ay, bx, by) => {
-		const a = this.cellCentreToPos(ax, ay)
-			, b = this.cellCentreToPos(bx, by);
-
-		this._ctx.beginPath();
-		this._ctx.moveTo(a.x, a.y);
-		this._ctx.lineTo(b.x, b.y);
-		this._ctx.lineWidth = this._cellWidth / 5;
-		this._ctx.strokeStyle = 'magenta';
-		this._ctx.stroke();
-	}
 
 }
